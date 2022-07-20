3a803f73a98984ccd1ce37eeaa6ead81b109a758
shr26207 <sunhaoran@offcn.com>
2022-07-20 15:33:00 +0800

    ShardSingleFlight

--- singleflight/singleflight_test.go
+++ singleflight/singleflight_test.go
@@ -29,7 +29,7 @@ func BenchmarkSingleFlightSetParallel(b *testing.B) {
 
 func BenchmarkShardSingleFlightSetParallel(b *testing.B) {
 	g := NewShardSingleFlight()
-	tests := []string{"e", "a", "e", "a", "b", "c", "b", "a", "c", "d", "b", "c", "d"}
+	tests := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 	rand.Seed(time.Now().Unix())
 	b.RunParallel(func(pb *testing.PB) {
 		id := rand.Intn(len(tests))
@@ -43,6 +43,23 @@ func BenchmarkShardSingleFlightSetParallel(b *testing.B) {
 		}
 	})
 }
+func BenchmarkShardSingleFlightBadSetParallel(b *testing.B) {
+	g := NewShardSingleFlight()
+	tests := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	rand.Seed(time.Now().Unix())
+	b.RunParallel(func(pb *testing.PB) {
+		_ = rand.Intn(len(tests))
+		for pb.Next() {
+			_, err := g.Do(makeKey(tests, 0), func() (interface{}, error) {
+				return "Val", nil
+			})
+			if err != nil {
+				return
+			}
+		}
+	})
+}
 
 // go test -bench=SetParallel -benchmem -count=10 -cpuprofile profile.out ./. | tee old.txt
+// benchstat old.txt
 //go tool pprof profile.out
